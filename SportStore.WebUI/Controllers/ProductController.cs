c22d32c3077a4bae3ea1d6a05feb8c118ada9df3
arkiden nedikra <arkiden@hotmail.com>
2011-12-31 20:10:58 -0700

    added navigation

--- SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -32,7 +32,9 @@ namespace SportsStore.WebUI.Controllers
                     {
                         CurrentPage = page,
                         ItemsPerPage = PageSize,
-                        TotalItems = repository.Products.Count()
+                        TotalItems = category == null ?
+                            repository.Products.Count() :
+                            repository.Products.Where(e => e.Category == category).Count()
                     },
                     CurrentCategory = category
             };
